8102e9dca08b194aa9f2cbf01cef0e4fb321aa15
AnnMariyaKorah-Deakin <s226052211@deakin.edu.au>
2026-05-08 17:31:40 +1000

    Initial project setup

---
+++ src/main/java/com/deakin/__SIT753_7_1_C/Application.java
@@ -0,0 +1,13 @@
+package com.deakin.__SIT753_7_1_C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/deakin/__SIT753_7_1_C/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deakin.__SIT753_7_1_C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
